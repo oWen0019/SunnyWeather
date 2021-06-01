cd8d45eee2ae34336ba227b92ffa0b62b4c5285d
CodeDolphin <codedolphin@gmail.com>
2021-06-01 20:48:01 +0800

    MaterialCard bug修复

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -11,6 +11,7 @@ import retrofit2.http.Query
  */
 interface PlaceService {
 
+    //https://api.caiyunapp.com/v2/place?query=北京&token={token}&lang=zh_CN
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 }
--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -1,13 +1,11 @@
 package com.sunnyweather.android.ui.place
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -37,31 +35,20 @@ class PlaceFragment : Fragment() {
         recyclerView.layoutManager = layoutManager
         adapter = PlaceAdapter(this, viewModel.placeList)
         recyclerView.adapter = adapter
-        searchPlaceEdit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
+        searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
             }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
+        }
 
 
-            override fun afterTextChanged(s: Editable?) {
-                val content = s.toString()
-                if (content.isNotEmpty()) {
-                    try {
-                        viewModel.searchPlaces(content)
-                    } catch (e: Exception) {
-                        Log.d("e", "afterTextChanged: $e")
-                    }
-                } else {
-                    recyclerView.visibility = View.GONE
-                    bgImageView.visibility = View.VISIBLE
-                    viewModel.placeList.clear()
-                    adapter.notifyDataSetChanged()
-                }
-            }
-        })
 
         viewModel.placeLiveData.observe(this, Observer { result ->
             val places = result.getOrNull()
